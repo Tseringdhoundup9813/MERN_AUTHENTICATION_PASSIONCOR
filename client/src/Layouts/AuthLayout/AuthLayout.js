9a64b3217e38ea1b123f3e1e8b7fa36431869682
TseringDhoundup1997 <47768382+Tseringdhoundup9813@users.noreply.github.com>
2023-04-21 23:07:05 +0545

    routing setup almost finished

---
+++ client/src/Layouts/AuthLayout/AuthLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Authlayout.css"
+
+function AuthLayout() {
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export default AuthLayout
---
+++ client/src/Layouts/ProfileLayout/Profilelayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./profilelayout.css"
+
+function Profilelayout() {
+  return (
+    <div>Profilelayout</div>
+  )
+}
+
+export default Profilelayout
